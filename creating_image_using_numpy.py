a065dc7db494ae1cd2570d24521fd1bdea063bfd
Arijit2003 <arijitmodak2003@gmail.com>
2023-06-09 01:14:32 +0530

    Using only cv2 and numpy

--- creating_image_using_numpy.py
+++ creating_image_using_numpy.py
@@ -8,9 +8,8 @@ image[:,:] = 100
 image=image[:,:]+10
 image[200:300,300:400] = 255
 print(image[200:300,300:400])
-#cv2.imwrite("F:\FallSemester23to24\ComputerVision\Codes\image.jpg",image)
-img=cv2.imread("F:\FallSemester23to24\ComputerVision\Codes\image.jpg")
-plt.imshow(img)
-plt.show()
+cv2.imwrite("F:\FallSemester23to24\ComputerVision\Codes\image.jpg",image)
+cv2.imread("F:\FallSemester23to24\ComputerVision\Codes\image.jpg")
+cv2.waitKey(5000)
 
 
